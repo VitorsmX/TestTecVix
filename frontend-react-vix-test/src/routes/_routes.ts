b57a9b37ca79beb38f4b916ee81c9767ccabebef
Vitor Sousa Mesquita <72332412+VitorsmX@users.noreply.github.com>
2026-01-11 20:27:43 -0300

    feat: adiciona tela de login no frontend

--- frontend-react-vix-test/src/routes/_routes.ts
+++ frontend-react-vix-test/src/routes/_routes.ts
@@ -3,15 +3,15 @@ import { HomeRouter } from "./HomeRoute";
 import { MyVMsRouter } from "./MyVMsRouter";
 import { VirtualMachineRouter } from "./VirtualMachineRouter";
 import { MSPRegisterRouter } from "./MSPRegisterRouter";
-import { RegisterRouter } from "./RegisterRouter";
+//import { RegisterRouter } from "./RegisterRouter";
 import { LoginRouter } from "./LoginRouter";
 import { WhiteLabelRouter } from "./WhiteLabelRouter";
 
 export const mainRoutes = [
   DefaultRouter,
   HomeRouter,
-  // LoginRouter, // Descomentar para renderizar o login
-  // RegisterRouter, // Descomentar para renderizar o register
+  LoginRouter, // Descomentar para renderizar o login
+  //RegisterRouter, // Descomentar para renderizar o register
   VirtualMachineRouter,
   MyVMsRouter,
   MSPRegisterRouter,
