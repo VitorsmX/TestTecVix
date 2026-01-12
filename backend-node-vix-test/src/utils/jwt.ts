5dedac0f57d7b0c0635178773e551dd4a0387515
Vitor Sousa Mesquita <72332412+VitorsmX@users.noreply.github.com>
2026-01-11 22:53:58 -0300

    feat: adiciona autenticação jwt no backend

--- backend-node-vix-test/src/utils/jwt.ts
+++ backend-node-vix-test/src/utils/jwt.ts
@@ -1,16 +1,33 @@
 import jwt, { TokenExpiredError } from "jsonwebtoken";
-// import { AppError } from "../errors/AppError";
+import { AppError } from "../errors/AppError";
+import { ERROR_MESSAGE } from "../constants/erroMessages";
+import { STATUS_CODE } from "../constants/statusCode";
 
-const secret = process.env.JWT_SECRET;
+const secret = process.env.JWT_SECRET as string;
 
-interface IPayload {}
+if (!secret) {
+  throw new Error("JWT_SECRET not defined");
+}
 
-export const genToken = (payload: IPayload) => {};
+interface IPayload {
+  sub: string;
+}
 
-export const verifyToken = (token: string) => {
+export const genToken = (payload: IPayload): string => {
+  return jwt.sign(payload, secret, {
+    expiresIn: "1d",
+  });
+};
+
+export const verifyToken = (token: string): IPayload => {
   try {
-    return; // data;
+    const decoded = jwt.verify(token, secret) as IPayload;
+    return decoded;
   } catch (error) {
-    // throws new AppError(ERROR_MESSAGE.INVALID_TOKEN, STATUS_CODE.UNAUTHORIZED);
+    if (error instanceof TokenExpiredError) {
+      throw new AppError(ERROR_MESSAGE.CODE_EXPIRED, STATUS_CODE.UNAUTHORIZED);
+    }
+
+    throw new AppError(ERROR_MESSAGE.INVALID_TOKEN, STATUS_CODE.UNAUTHORIZED);
   }
 };
